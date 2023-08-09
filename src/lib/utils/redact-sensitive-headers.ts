cb76262dda054a33a7d5b4abdcb2a6242bf3e4c3
Andrey Melikhov <melikhov@yandex-team.ru>
2023-08-09 18:03:06 +0300

    feat: Add headers redacter

---
+++ src/lib/utils/redact-sensitive-headers.ts
@@ -0,0 +1,31 @@
+import {IncomingHttpHeaders} from 'http';
+import {Dict} from '../../types';
+import {prepareSensitiveKeysRedacter} from './redact-sensitive-keys';
+
+export type SensitiveHeadersRedacter = (inputHeaders: Dict | IncomingHttpHeaders) => Dict;
+
+export default function prepareSensitiveHeadersRedacter(
+    sensitiveHeaders: Array<string> = [],
+    headersWithSensitiveUrls: Array<string> = [],
+    redactSensitiveQueryParams: (input: string) => string = (input) => input,
+    isDevMode = false,
+) {
+    const redactSensitiveHeaders: SensitiveHeadersRedacter = (inputHeaders) => {
+        if (isDevMode) {
+            return inputHeaders;
+        }
+
+        const redactSensitiveKeys = prepareSensitiveKeysRedacter(sensitiveHeaders);
+
+        const result = redactSensitiveKeys(inputHeaders);
+
+        Object.keys(result).forEach((headerName) => {
+            if (headersWithSensitiveUrls.includes(headerName.toLowerCase())) {
+                result[headerName] = redactSensitiveQueryParams(result[headerName] as string);
+            }
+        });
+
+        return result;
+    };
+    return redactSensitiveHeaders;
+}
