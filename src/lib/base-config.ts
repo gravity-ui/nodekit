3e9fe399b882943e274012dca05337182f2ec422
Andrey Melikhov <melikhov@yandex-team.ru>
2023-08-10 11:16:35 +0300

    fix: revert nkDefaultSensitiveKeys

--- src/lib/base-config.ts
+++ src/lib/base-config.ts
@@ -1,7 +1,7 @@
 import {AppConfig} from '../types';
 
 export const NODEKIT_BASE_CONFIG: AppConfig = {
-    nkDefaultSensitiveKeys: ['password'],
+    nkDefaultSensitiveKeys: ['authorization', 'cookie', 'set-cookie', 'password'],
     nkDefaultSensitiveHeaders: ['authorization', 'cookie', 'set-cookie', 'password'],
     nkDefaultHeadersWithSensitiveUrls: ['referer'],
     nkDefaultSensitiveQueryParams: [],
