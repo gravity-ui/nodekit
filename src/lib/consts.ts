033d71b4f38c6290d836ab6be8e45259ef357586
Ivan Lediaev <ravestar@yandex-team.ru>
2023-12-06 20:00:24 +0300

    feat: add userId context param for legacy compatibility

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,3 +1,4 @@
 export const REQUEST_ID_HEADER = 'x-request-id';
 export const REQUEST_ID_PARAM_NAME = 'requestId';
+export const USER_ID_PARAM_NAME = 'userId';
 export const TRACE_KEY = 'uber-trace-id';
