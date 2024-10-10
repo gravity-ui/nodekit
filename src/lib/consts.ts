acc8dde5d4e4b210948988fc04e7243df667af85
Andrey Melikhov <melikhov@yandex-team.ru>
2024-10-10 16:10:47 +0300

    feat: add re-export public consts (#55)
    * feat: add re-export public consts
    * fix: typo

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,6 +1,3 @@
-export const REQUEST_ID_HEADER = 'x-request-id';
-export const REQUEST_ID_PARAM_NAME = 'requestId';
-export const USER_ID_PARAM_NAME = 'userId';
-export const USER_LANGUAGE_PARAM_NAME = 'userLang';
+export * from './public-consts';
 export const TRACE_KEY = 'uber-trace-id';
 export const REDACTED_STRING = '[REDACTED]';
