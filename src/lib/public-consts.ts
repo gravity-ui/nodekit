b8677230a3443dce3267d3368e36745f008410d7
melikhov <melikhov@yandex-team.ru>
2024-10-10 15:22:30 +0300

    feat: add re-export public consts

---
+++ src/lib/public-consts.ts
@@ -0,0 +1,4 @@
+export const REQUEST_ID_HEADER = 'x-request-id';
+export const REQUEST_ID_PARAM_NAME = 'requestId';
+export const USER_ID_PARAM_NAME = 'userId';
+export const USER_LANGUAGE_PARAM_NAME = 'userLang';
