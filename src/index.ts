3fe14874743c2e402e3f532a7040efb745f86505
Andrey Melikhov <melikhov@yandex-team.ru>
2024-12-03 16:54:40 +0300

    feat: export SpanKind (#66)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {NodeKit} from './nodekit';
 export {AppContext} from './lib/context';
-export {AppConfig, AppContextParams, AppDynamicConfig} from './types';
+export {AppConfig, AppContextParams, AppDynamicConfig, SpanKind} from './types';
 export {AppError} from './lib/app-error';
 export {DynamicConfigSetup} from './lib/dynamic-config-poller';
 export {NodeKitLogger} from './lib/logging';
