c84145c8df766d2b0abcbbd7092b8742c3278d20
Kirill Kharitonov <kirkharitonov@qavm-477c8d9e.qemu>
2025-09-18 18:30:42 +0300

    feat: export prepareClickhouseClient function

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export {AppError} from './lib/app-error';
 export {DynamicConfigSetup} from './lib/dynamic-config-poller';
 export {NodeKitLogger} from './lib/logging';
 export {initTracing} from './lib/tracing/init-tracing';
+export {prepareClickhouseClient} from './lib/telemetry/clickhouse';
 export * from './lib/public-consts';
