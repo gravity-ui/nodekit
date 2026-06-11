7b8b59928ac6544096005240e667564d05cfee68
vershkovdenis <vershkovdenis@yandex-team.ru>
2026-06-11 06:48:50 +0300

    feat: add csrf token to the list of sensitiveHeaders

--- src/lib/base-config.ts
+++ src/lib/base-config.ts
@@ -2,7 +2,13 @@ import {AppConfig} from '../types';
 
 export const NODEKIT_BASE_CONFIG: AppConfig = {
     nkDefaultSensitiveKeys: ['authorization', 'cookie', 'set-cookie', 'password'],
-    nkDefaultSensitiveHeaders: ['authorization', 'cookie', 'set-cookie', 'password'],
+    nkDefaultSensitiveHeaders: [
+        'authorization',
+        'cookie',
+        'set-cookie',
+        'password',
+        'x-csrf-token',
+    ],
     nkDefaultHeadersWithSensitiveUrls: ['referer'],
     nkDefaultSensitiveQueryParams: [],
 };
