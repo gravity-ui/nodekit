8ec4cee26d944550ec5595de652fa0fa36f69296
Volodomor <volodilnik@gmail.com>
2026-07-30 10:57:35 +0200

    fix(config): change priority and add tests

--- src/nodekit.ts
+++ src/nodekit.ts
@@ -77,18 +77,18 @@ export class NodeKit {
         const fileConfig: AppConfig = loadFileConfigs(options.configsPath, appInstallation, appEnv);
 
         const appName =
-            process.env.APP_NAME || fileConfig.appName || options.config?.appName || 'namelessApp';
+            process.env.APP_NAME || options.config?.appName || fileConfig.appName || 'namelessApp';
 
         const appVersion =
             process.env.APP_VERSION ||
-            fileConfig.appVersion ||
             options.config?.appVersion ||
+            fileConfig.appVersion ||
             'versionlessApp';
 
         const appLoggingLevel =
             process.env.APP_LOGGING_LEVEL ||
-            fileConfig.appLoggingLevel ||
-            options.config?.appLoggingLevel;
+            options.config?.appLoggingLevel ||
+            fileConfig.appLoggingLevel;
 
         this.config = Object.assign({}, NODEKIT_BASE_CONFIG, fileConfig, options.config || {}, {
             appName,
